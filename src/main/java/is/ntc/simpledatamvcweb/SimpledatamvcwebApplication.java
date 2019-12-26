15cd3be84546f3893dcfe8c7619d1dce1da1ee80
saltukhov <is1ntc@gmail.com>
2019-12-26 10:14:05 +0200

    Initial commit

---
+++ src/main/java/is/ntc/simpledatamvcweb/SimpledatamvcwebApplication.java
@@ -0,0 +1,13 @@
+package is.ntc.simpledatamvcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpledatamvcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpledatamvcwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/ntc/simpledatamvcweb/SimpledatamvcwebApplicationTests.java
@@ -0,0 +1,13 @@
+package is.ntc.simpledatamvcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpledatamvcwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
